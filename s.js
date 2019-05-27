af14b68b17a97817283d38182c33d7744ef3dbd3
Nesmon <nesmon2108@protonmail.com>
2019-05-27 08:54:40 +0200

    Switc to try catch for get information

--- s.js
+++ s.js
@@ -1,31 +1,39 @@
 let net = require('net');
- 
-let server = net.createServer(function (socket) {
- 
-    socket.on('data', function (data) {
-        if(!!data){
-            let stringData = data.toString('utf8').trim();
-            if(!!stringData.length && !!stringData.length && stringData.length > 0){
-                try{
-                  let matchRegex = stringData.match('\{" *livepp_current_pp" *: *"([0-9\.]+)" *\}');
-                  if(matchRegex != null && matchRegex.length > 1){
-                    console.log("matchFound:"+matchRegex[1]);
-                  }else{
-                    console.warn("could not extract livepp_current_app");
-                  }
-                 
-                } catch(e){
-                  console.error("Unexpected error :"+e+"with stringData :"+stringData);
-                }
-              }else{
-                console.warn("empty String received");
-              }
- 
-        }else{
-          console.warn("data is null");
-        }
-    });
- 
-});
 
-server.listen(7840, '0.0.0.0');
+let server = net.createServer(function (socket) {
+
+      socket.on('data', function (data) {
+        try {
+          textChunk = data.toString('utf8');
+          //chunk = JSON.parse(data);
+          console.log(textChunk);
+        } catch (e) {
+          console.log(e);
+        }
+
+        //     if(!!data){
+        //         let stringData = data.toString('utf8').trim();
+        //         if(!!stringData.length && !!stringData.length && stringData.length > 0){
+        //             try{
+        //               let matchRegex = stringData.match('\{" *livepp_current_pp" *: *"([0-9\.]+)" *\}');
+        //               if(matchRegex != null && matchRegex.length > 1){
+        //                 console.log("matchFound:"+matchRegex[1]);
+        //               }else{
+        //                 console.warn("could not extract livepp_current_app");
+        //               }
+
+        //             } catch(e){
+        //               console.error("Unexpected error :"+e+"with stringData :"+stringData);
+        //             }
+        //           }else{
+        //             console.warn("empty String received");
+        //           }
+
+        //     }else{
+        //       console.warn("data is null");
+        //     }
+         });
+
+      });
+
+      server.listen(7840, '0.0.0.0');
